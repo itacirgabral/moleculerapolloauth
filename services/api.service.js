1b14a4efee847984ca016ec660d3228a06e1564e
itacir gabral <mu@localhost.localdomain>
2019-12-05 19:49:17 -0400

    typos

--- services/api.service.js
+++ services/api.service.js
@@ -7,25 +7,16 @@ module.exports = {
     name: "api",
 
     mixins: [
-        // Gateway
         ApiGateway,
 
-        // GraphQL Apollo Server
         ApolloService({
-
-            // Global GraphQL typeDefs
             typeDefs: ``,
-
-            // Global resolvers
             resolvers: {},
-
-            // API Gateway route options
             routeOptions: {
                 path: "/graphql",
                 cors: true,
                 mappingPolicy: "restrict"
             },
-
             // https://www.apollographql.com/docs/apollo-server/v2/api/apollo-server.html
             serverOptions: {
                 tracing: true,
